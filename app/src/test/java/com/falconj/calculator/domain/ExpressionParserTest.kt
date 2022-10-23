d984f46fb2c7f7df0cbcb451f71c5702684d4845
Justin Lin <justinranger24@gmail.com>
2022-10-23 14:07:30 +0800

    Add ExpressionParser.kt

---
+++ app/src/test/java/com/falconj/calculator/domain/ExpressionParserTest.kt
@@ -0,0 +1,33 @@
+package com.falconj.calculator.domain
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ExpressionParserTest {
+
+    private lateinit var parser: ExpressionParser
+
+    @Test
+    fun `All properly parsed`() {
+        parser = ExpressionParser("2.901/0.347+6-(5.8x2)%2")
+
+        val actual = parser.parse()
+
+        val expected = listOf(
+            ExpressionPart.Number(2.901),
+            ExpressionPart.Op(Operation.DIVIDE),
+            ExpressionPart.Number(0.347),
+            ExpressionPart.Op(Operation.ADD),
+            ExpressionPart.Number(6.0),
+            ExpressionPart.Op(Operation.SUBTRACT),
+            ExpressionPart.Parentheses(ParenthesesType.Opening),
+            ExpressionPart.Number(5.8),
+            ExpressionPart.Op(Operation.MULTIPLY),
+            ExpressionPart.Number(2.0),
+            ExpressionPart.Parentheses(ParenthesesType.Closing),
+            ExpressionPart.Op(Operation.PERCENT),
+            ExpressionPart.Number(2.0)
+        )
+        assertThat(expected).isEqualTo(actual)
+    }
+}
