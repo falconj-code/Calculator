4f37d5312e164a6fd08e796b4e65f33cc4d84ee2
Justin Lin <justinranger24@gmail.com>
2022-10-23 23:58:42 +0800

    Add ExpressionEvaluator.kt

--- app/src/test/java/com/falconj/calculator/domain/ExpressionParserTest.kt
+++ app/src/test/java/com/falconj/calculator/domain/ExpressionParserTest.kt
@@ -9,24 +9,26 @@ class ExpressionParserTest {
 
     @Test
     fun `All properly parsed`() {
-        parser = ExpressionParser("2.901/0.347+6-(5.8x2)%2")
+        parser = ExpressionParser("2.901+0.47+(6/3)-(5.8x2)")
 
         val actual = parser.parse()
 
         val expected = listOf(
             ExpressionPart.Number(2.901),
-            ExpressionPart.Op(Operation.DIVIDE),
-            ExpressionPart.Number(0.347),
             ExpressionPart.Op(Operation.ADD),
+            ExpressionPart.Number(0.47),
+            ExpressionPart.Op(Operation.ADD),
+            ExpressionPart.Parentheses(ParenthesesType.Opening),
             ExpressionPart.Number(6.0),
+            ExpressionPart.Op(Operation.DIVIDE),
+            ExpressionPart.Number(3.0),
+            ExpressionPart.Parentheses(ParenthesesType.Closing),
             ExpressionPart.Op(Operation.SUBTRACT),
             ExpressionPart.Parentheses(ParenthesesType.Opening),
             ExpressionPart.Number(5.8),
             ExpressionPart.Op(Operation.MULTIPLY),
             ExpressionPart.Number(2.0),
-            ExpressionPart.Parentheses(ParenthesesType.Closing),
-            ExpressionPart.Op(Operation.PERCENT),
-            ExpressionPart.Number(2.0)
+            ExpressionPart.Parentheses(ParenthesesType.Closing)
         )
         assertThat(expected).isEqualTo(actual)
     }
